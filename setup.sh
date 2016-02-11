d8c0ff698a26d545930c15bb0defca48943d2e1d
Dan Yasny <dyasny@gmail.com>
2016-02-11 09:45:07 -0500

    total refactoring for vars and virtualenv deployment

--- setup.sh
+++ setup.sh
@@ -3,19 +3,17 @@
 # Configure the system to be used with ansible 2 and shade
 
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-#sudo yum -y install ansible --enablerepo=epel-testing
-sudo yum -y install python-pip
+sudo yum -y install python-pip python-virtualenv
 sudo yum-config-manager --disable epel
 
+#create a virtual env for ansible
+cd $HOME
+virtualenv ansible
+source $HOME/ansible/bin/activate
+
 #install ansible and shade from pip
+sudo pip install ansible shade funcsigs
 
-sudo pip install ansible
-sudo pip install shade
-
-#shade from pip breaks python-openstackclient, it needs to be 
-#reinstalled to become operational again
-sudo yumdownloader python-openstackclient
-sudo rpm -ivh $(ls -A |grep python-openstackclient) --force
 
 echo Setup complete.
 echo To create objects run create.yml
